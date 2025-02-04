a56d3e92d513bdbfba927d1122b161c7e802d402
Yogesh <114099821+YogeshBabyOP@users.noreply.github.com>
2025-02-04 21:37:13 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-
   return (
-    <div>
-      Hello There Vite + React + Tailwind here
-    </div>
-  )
+    <>
+      <div>Hello There Vite + React + Tailwind here</div>
+
+      <h1>Modified by yogesh</h1>
+    </>
+  );
 }
 
-export default App
+export default App;
